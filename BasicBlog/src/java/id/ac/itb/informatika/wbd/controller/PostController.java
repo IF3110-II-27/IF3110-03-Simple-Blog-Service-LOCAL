53eaa1fc3cc571a32b7042f3a6379fceffe7af8e
Ichlasul Amal <ichlasul.a@gmail.com>
2014-12-18 15:08:29 +0700

    Post update now uses Firebase

--- BasicBlog/src/java/id/ac/itb/informatika/wbd/controller/PostController.java
+++ BasicBlog/src/java/id/ac/itb/informatika/wbd/controller/PostController.java
@@ -5,8 +5,6 @@ import id.ac.itb.informatika.wbd.model.Post;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,9 +16,6 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.MultivaluedHashMap;
-import javax.ws.rs.core.MultivaluedMap;
 import org.json.JSONObject;
 
 @ManagedBean
@@ -42,8 +37,6 @@ public class PostController {
     }
     
     public PostController(){
-        post = new Post();
-        
         publishedPosts = new ArrayList<>();
         draftPosts = new ArrayList<>();
         deletedPosts = new ArrayList<>();
@@ -61,20 +54,20 @@ public class PostController {
             String key = keys.next();
             JSONObject val = json.getJSONObject(key);
             
-            pos = new Post();
-            pos.setId(key);
-            pos.setTitle(val.getString("title"));
-            pos.setContent(val.getString("content"));
-            pos.setPublished(val.getString("published"));
-            pos.setDate(val.getString("date"));
-            pos.setDeleted(val.getString("deleted"));
+            post = new Post();
+            post.setId(key);
+            post.setTitle(val.getString("title"));
+            post.setContent(val.getString("content"));
+            post.setPublished(val.getString("published"));
+            post.setDate(val.getString("date"));
+            post.setDeleted(val.getString("deleted"));
             
-            if(pos.getDeleted().equalsIgnoreCase("true")){
-                deletedPosts.add(pos);
-            }else if(pos.getPublished().equalsIgnoreCase("true")){
-                publishedPosts.add(pos);
+            if(post.getDeleted().equalsIgnoreCase("true")){
+                deletedPosts.add(post);
+            }else if(post.getPublished().equalsIgnoreCase("true")){
+                publishedPosts.add(post);
             }else{
-                draftPosts.add(pos);
+                draftPosts.add(post);
             }
         }
     }
@@ -163,7 +156,7 @@ public class PostController {
     
     public String delete(String id, int index){
         Firebase fb = new Firebase("https://if3110-iii-27.firebaseio.com/");
-        Map<String, Object> val = new HashMap<String, Object>();
+        Map<String, Object> val = new HashMap<>();
         val.put("deleted", "true");
         fb.child("posts").child(id).updateChildren(val);
         
@@ -172,7 +165,7 @@ public class PostController {
     
     public String restore(String id){
         Firebase fb = new Firebase("https://if3110-iii-27.firebaseio.com/");
-        Map<String, Object> val = new HashMap<String, Object>();
+        Map<String, Object> val = new HashMap<>();
         val.put("deleted", "false");
         fb.child("posts").child(id).updateChildren(val);
         
@@ -188,74 +181,25 @@ public class PostController {
     
     public String publish(String id){
         Firebase fb = new Firebase("https://if3110-iii-27.firebaseio.com/");
-        Map<String, Object> val = new HashMap<String, Object>();
+        Map<String, Object> val = new HashMap<>();
         val.put("published", "true");
         fb.child("posts").child(id).updateChildren(val);
         
         return "post_draft?faces-redirect=true";
     }
     
-   private Post pos;
-   
-    public Post getPos(){
-        return pos;
-    }
-
-    public void setPos(Post pos){
-        this.pos = new Post();
-        this.pos.setId(pos.getId());
-        this.pos.setTitle(pos.getTitle());
-        this.pos.setContent(pos.getContent());
-        this.pos.setPublished(pos.getPublished());
-        this.pos.setDate(pos.getDate());
-    }
-    
-    public void edit(){
-        Connection con = null;
-        String url = "jdbc:mysql://localhost:3306/simpleblog";
-        String user = "root";
-        String driver = "com.mysql.jdbc.Driver";
-        String password = "";
+    public void update(){
+        Firebase fb = new Firebase("https://if3110-iii-27.firebaseio.com/");
+        Map<String, Object> val = new HashMap<>();
+        val.put("title", post.getTitle());
+        val.put("date", post.getDate());
+        val.put("content", post.getContent());
+        fb.child("posts").child(id).updateChildren(val);
+        
         try {
-            Class.forName(driver).newInstance();            
-            con = DriverManager.getConnection(url, user, password);
-            String query = "UPDATE post SET Judul=?, Konten=?, Tanggal=? WHERE id=?";
-            PreparedStatement ps = con.prepareStatement(query);
-            ps.setString(1, pos.getTitle());
-            ps.setString(2, pos.getContent());            
-            ps.setString(3, pos.getDate());
-           // ps.setInt(4, pos.getId());               
-            ps.executeUpdate();
             FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
-            con.close();
-        } catch (IOException | ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-    
-    public void execute(String id){
-        Connection con = null;
-        String url = "jdbc:mysql://localhost:3306/simpleblog";
-        String user = "root";
-        String driver = "com.mysql.jdbc.Driver";
-        String password = "";        
-        try {
-            Class.forName(driver).newInstance();
-            con = DriverManager.getConnection(url, user, password);
-            PreparedStatement ps = con.prepareStatement("SELECT * FROM post WHERE id="+id);
-            ResultSet res = ps.executeQuery();
-            while(res.next()){
-                pos.setTitle(res.getString("Judul"));
-                pos.setContent(res.getString("Konten"));
-                //pos.setStatus(res.getString("Status"));
-                pos.setDate(res.getString("Tanggal"));
-                //pos.setDeleted(res.getInt("deleted"));
-                //pos.setId(id);
-                this.id = id;
-            }            
-            con.close();
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
-            System.out.println(ex.getMessage());
+        } catch (IOException ex) {
+            Logger.getLogger(PostController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
